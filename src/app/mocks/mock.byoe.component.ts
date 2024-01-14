dd32a9dabd0c660f99db9507736b21a2fdfa3039
Iain Soars <iain@schweben.org>
2024-01-14 19:21:10 +0000

    Removd more Angular

--- src/app/mocks/mock.byoe.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ex-byoe',
-    template: '',
-})
-export class MockByoeComponent {
-}
--- src/app/mocks/mock.categorised-excuse.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ex-categorised-excuse',
-    template: '',
-})
-export class MockCategorisedExcuseComponent {
-}
